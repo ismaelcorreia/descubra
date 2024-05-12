b009e7e5bbe2885443f08ccdd4da3d4659759aaf
ismaelcorreia <ismaelmyname@hotmail.com>
2024-05-12 16:17:02 +0100

    Register

--- app/(full-page)/layout.tsx
+++ app/(full-page)/layout.tsx
@@ -1,21 +1,22 @@
 import { Metadata } from 'next';
-import AppConfig from '../../layout/AppConfig';
+// import AppConfig from '../../layout/AppConfig';
 import React from 'react';
 
+import '@flaticon/flaticon-uicons/css/all/all.css';
 interface SimpleLayoutProps {
     children: React.ReactNode;
 }
 
 export const metadata: Metadata = {
-    title: 'PrimeReact Sakai',
-    description: 'The ultimate collection of design-agnostic, flexible and accessible React UI Components.'
+    title: 'Travel Finder',
+    description: 'Viaje com confiança.'
 };
 
 export default function SimpleLayout({ children }: SimpleLayoutProps) {
     return (
         <React.Fragment>
             {children}
-            <AppConfig simple />
+            {/* <AppConfig simple /> */}
         </React.Fragment>
     );
 }
